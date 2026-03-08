21f4fb39b8cc2a430c36ace1d648ded5e937402a
ColinVenancio <232087417+colinven@users.noreply.github.com>
2026-03-07 19:13:10 -0500

    implement builder pattern for property response for readability

--- server/response/propertyResponse.js
+++ server/response/propertyResponse.js
@@ -1,9 +1,56 @@
 class PropertyResponse {
-    constructor(successful, data, message) {
+    constructor(successful = null, data = null, message = null, price = null) {
         this.successful = successful;
         this.data = data;
         this.message = message;
+        this.price = price;
+    }
+    
+    success(boolean) {
+        this.successful = boolean;
+    }
+
+    data(jsonData) {
+        this.data = jsonData;
+    }
+
+    message(messageString) {
+        this.message = msg;
+    }
+
+    price(priceArray) {
+        this.price = priceArray;
     }
 }
 
-module.exports = PropertyResponse;
+class PropertyResponseBuilder {
+    constructor() {
+        this.propertyResponse = new PropertyResponse();
+    }
+
+    success(boolean) {
+        this.propertyResponse.successful = boolean;
+        return this;
+    }
+
+    data(jsonData) {
+        this.propertyResponse.data = jsonData;
+        return this;
+    }
+
+    message(messageString) {
+        this.propertyResponse.message = messageString;
+        return this;
+    }
+
+    price(priceArray) {
+        this.propertyResponse.price = priceArray;
+        return this;
+    }
+
+    build() {
+        return this.propertyResponse;
+    }
+
+}
+module.exports = { PropertyResponse, PropertyResponseBuilder };
